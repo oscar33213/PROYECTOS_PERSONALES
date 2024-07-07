de44902492869a2b0b42854423da9a31c7e9a9e2
oscar33213 <133204943+oscar33213@users.noreply.github.com>
2024-07-07 22:02:36 +0200

    commit whats

--- PYTHON/Botwhats.py
+++ PYTHON/Botwhats.py
@@ -1,13 +1,29 @@
 import pywhatkit
 from datetime import datetime
 import time
+import random
 
+# Lista de posibles mensajes
+posibles_mensajes = [
+    'Hola, ¿cómo estás?',
+    '¡Feliz cumpleaños!',
+    'Nos vemos mañana a las 10.',
+    'Reunión confirmada para el lunes.',
+    'Espero que tengas un buen día.',
+    'No olvides la reunión de hoy.',
+    '¡Buen trabajo con el proyecto!',
+    'Te llamaré más tarde.'
+]
+
+# Lista de números de teléfono
+numeros = []
+
+# Tiempo actual más 60 segundos
 seconds = time.time() + 60
 date = datetime.fromtimestamp(seconds)
-pywhatkit.sendwhatmsg('+34xxxxxx', 'Hola', date.hour, date.minute)
 
-
-time.sleep(5)
-
-
-input('Presiona uuna tecla para salir')
+# Enviar mensajes
+for numero in numeros:
+    mensaje = random.choice(posibles_mensajes)  # Seleccionar un mensaje aleatorio
+    pywhatkit.sendwhatmsg(numero, mensaje, date.hour, date.minute)
+    time.sleep(5)  # Asegura un intervalo de 5 segundos entre cada mensaje
