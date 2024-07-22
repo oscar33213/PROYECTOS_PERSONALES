d59bc520c311ffa125514daa2a2fbe0115a2ecdf
oscar33213 <133204943+oscar33213@users.noreply.github.com>
2024-07-22 20:57:13 +0200

    we

--- JS-TS/MFLIX/src/constantes.ts
+++ JS-TS/MFLIX/src/constantes.ts
@@ -1,5 +1,5 @@
 export const flechas = {
-left: "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/7f4b7d5a25593dd2d2857f2aaa467c5fd87b5cbb/mflix/flecha-izquierda.svg",
+left:"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/7f4b7d5a25593dd2d2857f2aaa467c5fd87b5cbb/mflix/flecha-izquierda.svg",
 right:"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/7f4b7d5a25593dd2d2857f2aaa467c5fd87b5cbb/mflix/flecha-derecha.svg",
 };
 
